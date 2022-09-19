d3346528e593ae4826588c6b7b33953b72650486
KASHI TAMANG <tamang.kashi@gmail.com>
2022-09-19 16:50:33 -0700

    adding prim 2

--- day-01/index.js
+++ day-01/index.js
@@ -16,4 +16,26 @@ function titleCase(sentence) {
   return words.join(' ');
   // 'Alchemy Rocks Gold'
 }
-module.exports = { reverseSentence, titleCase };
+
+function oddishOrEvenish(number) {
+  //if sum of all digits is even = evenish
+  //if sum of all digits is odd = oddish
+  const sum = String(number)
+  //convert to string
+    .split('')
+    //return sum of all digits in the number
+    .reduce((acc, curr) => {
+      return acc + Number(curr);
+    }, 0);
+  //take the sum, if its modulo is even, return 'evenish'
+  if (sum % 2 == 0) {
+    return 'Evenish';
+    // if modulo is odd 'return oddish'
+  } else if (sum % 2 == 1) {
+    return 'Oddish';
+    //all other inputs considered invalid
+  } else {
+    return 'Invalid Entry';
+  }
+}
+module.exports = { reverseSentence, titleCase, oddishOrEvenish };
--- day-01/index.test.js
+++ day-01/index.test.js
@@ -1,4 +1,4 @@
-const { reverseSentence, titleCase } = require('./index');
+const { reverseSentence, titleCase, oddishOrEvenish } = require('./index');
 
 describe('primitive code challenges', () => {
   describe('reverseSentence', () => {
@@ -9,5 +9,11 @@ describe('primitive code challenges', () => {
 
   it('should capitalize the first letter of each word in a string', () => {
     expect(titleCase('alchemy rocks gold')).toBe('Alchemy Rocks Gold');
+  });
+
+  it('should determine whether a number is Evenish or Oddish', () => {
+    expect(oddishOrEvenish(121)).toBe('Evenish');
+    expect(oddishOrEvenish(41)).toBe('Oddish');
+    expect(oddishOrEvenish('todays lecture: 2pm')).toBe('Invalid Entry, try again');
   });
 });
