24afd77f269c820e9e5abe896f6309f474ada040
KASHI TAMANG <tamang.kashi@gmail.com>
2022-09-19 15:58:23 -0700

    Adding prim 1

--- day-01/index.js
+++ day-01/index.js
@@ -4,4 +4,16 @@ function reverseSentence(str) {
     .map((word) => word.split('').reverse().join(''))
     .join(' ');
 }
-module.exports = { reverseSentence };
+
+function titleCase(sentence) {
+  // sentence = 'alchemy is gold'
+  const words = sentence.split(' ');
+  // words = ['alchemy', 'rocks', 'gold']
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+  }
+  // words = ['Alchemy', 'Rocks', 'Gold']
+  return words.join(' ');
+  // 'Alchemy Rocks Gold'
+}
+module.exports = { reverseSentence, titleCase };
--- day-01/index.test.js
+++ day-01/index.test.js
@@ -1,9 +1,13 @@
-const { reverseSentence } = require('./index');
+const { reverseSentence, titleCase } = require('./index');
 
 describe('primitive code challenges', () => {
   describe('reverseSentence', () => {
     it('should reverse a sentence', () => {
       expect(reverseSentence('alchemy rocks gold')).toBe('ymehcla skcor dlog');
     });
+  });
+
+  it('should capitalize the first letter of each word in a string', () => {
+    expect(titleCase('alchemy rocks gold')).toBe('Alchemy Rocks Gold');
   });
 });
