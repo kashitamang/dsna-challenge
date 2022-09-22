c40ffdc673409ff884dbbbbb9df653949d295fd7
KASHI TAMANG <tamang.kashi@gmail.com>
2022-09-22 16:39:50 -0700

    adding challenge 01

--- day-03/index.test.js
+++ day-03/index.test.js
@@ -1,5 +1,18 @@
+const { BinaryTreeNode } = require('.');
+
 describe('node based data structures', () => {
-  it('example test', () => {
-    expect(1).toBe(1);
+  
+  it('should return new binary tree', () => {
+    const A = new BinaryTreeNode('A');
+    const B = new BinaryTreeNode('B');
+    const C = new BinaryTreeNode('C');
+    const D = new BinaryTreeNode('D');
+
+    B.add(A);
+    B.add(D);
+    B.add(C);
+
+    expect(B.left.value).toEqual('A');
+
   });
 });
